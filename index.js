212949243b86f849fba91d15d365e7c6cc7a58eb
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-19 03:47:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const users = [
+   { id: 1, name: "Tushar", email: "tushar@tushar.com" },
+   { id: 2, name: "Naruto", email: "naruto@naruto.com" },
+   { id: 3, name: "Messi", email: "messi@messi.com" },
+];
+
+app.get("/", (req, res) => {
+   res.send("Server is running successfully");
+});
+
+app.get("/users", (req, res) => {
+   res.send(users);
+});
+
+app.listen(port, () => {
+   console.log(`app running at port: ${port}`);
+});
