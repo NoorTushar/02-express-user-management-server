3fee9b5922040780bae6efcdf91663c08195c32b
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-19 06:03:20 +0600

    done. we learned how to send data to server using post method and at the same time getting it in UI using bangla style

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middleware:
+app.use(cors());
+app.use(express.json());
+
 const users = [
    { id: 1, name: "Tushar", email: "tushar@tushar.com" },
    { id: 2, name: "Naruto", email: "naruto@naruto.com" },
    { id: 3, name: "Messi", email: "messi@messi.com" },
 ];
-
-app.use(cors());
 
 app.get("/", (req, res) => {
    res.send("Server is running successfully");
@@ -17,6 +19,15 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
    res.send(users);
+});
+
+app.post("/users", (req, res) => {
+   console.log("post request hitting from server side");
+   console.log(req.body);
+   const newUser = req.body;
+   newUser.id = users.length + 1;
+   users.push(newUser);
+   res.send(newUser);
 });
 
 app.listen(port, () => {
